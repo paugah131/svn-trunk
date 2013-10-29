97b33c602a62efa9fa14329efeacf7969b659aba
paul.gabrielsson <paul.gabrielsson@gmail.com>
2013-10-29 11:44:36 +0000

    Project Paul_Gabrielsson (name exercise) finally, at long last, committed to AnkhSVN on Tuesday October 29th, 2013

---
+++ Project1/Paul_Gabrielsson.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+#include <cstdarg>
+#include <clocale>
+#include "Paul_Gabrielsson.h"
+using namespace std;
+
+int paul()
+{
+	string name;
+
+	cout << "What is your name?" << '\n' << '\n';
+	cin >> name;
+	cout << "\nMy name is ";
+	cout << name << '\n';
+	cin.clear();
+
+	return 0;
+}
